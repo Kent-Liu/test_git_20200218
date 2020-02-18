d43927b7aa7aabe98aec99cf3943702b53149d81
Kent Liu <kent@softarts.cc>
2020-02-18 15:08:16 +0800

    Initial Commit

---
+++ HelloGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloGit
+//
+//  Created by Kent Liu on 2020/2/18.
+//  Copyright © 2020 SoftArts.cc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ HelloGit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloGit
+//
+//  Created by Kent Liu on 2020/2/18.
+//  Copyright © 2020 SoftArts.cc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // Setup code that might create autoreleased objects goes here.
+  }
+  return NSApplicationMain(argc, argv);
+}
