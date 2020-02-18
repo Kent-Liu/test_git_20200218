3ce18ef2b10498bcd868d090c4e8f7a629f62582
Kent Liu <kent@softarts.cc>
2020-02-18 15:13:05 +0800

    1.Create TestObject.

---
+++ HelloGit/TestObject.h
@@ -0,0 +1,17 @@
+//
+//  TestObject.h
+//  HelloGit
+//
+//  Created by Kent Liu on 2020/2/18.
+//  Copyright © 2020 SoftArts.cc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestObject : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HelloGit/TestObject.m
@@ -0,0 +1,13 @@
+//
+//  TestObject.m
+//  HelloGit
+//
+//  Created by Kent Liu on 2020/2/18.
+//  Copyright © 2020 SoftArts.cc. All rights reserved.
+//
+
+#import "TestObject.h"
+
+@implementation TestObject
+
+@end
